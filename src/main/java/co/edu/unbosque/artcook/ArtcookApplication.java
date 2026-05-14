15a67c0fb060776250e92b249f830b676c8bcf6a
juanfe11 <64513341+juanfe11@users.noreply.github.com>
2026-05-14 12:02:26 -0500

    Update ArtcookApplication.java
    version unificada cami-juan

--- src/main/java/co/edu/unbosque/artcook/ArtcookApplication.java
+++ src/main/java/co/edu/unbosque/artcook/ArtcookApplication.java
@@ -1,25 +1,42 @@
-// clase modificada camila-juan
-
 package co.edu.unbosque.artcook;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.modelmapper.ModelMapper;
 
+/**
+ * Clase principal que inicia la aplicación de Spring Boot.
+ * Se encarga de ejecutar el proyecto y configurar los beans necesarios.
+ */
 @SpringBootApplication
 public class ArtcookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArtcookApplication.class, args);
-	}
+    /**
+     * Método principal que arranca la aplicación.
+     *
+     * @param args argumentos de ejecución
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ArtcookApplication.class, args);
+    }
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    /**
+     * Crea y retorna una instancia de ModelMapper para mapeo entre entidades y DTOs.
+     *
+     * @return instancia de ModelMapper
+     */
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
 
+	/**
+ * Crea y retorna una instancia de BCryptPasswordEncoder para el cifrado seguro de contraseñas.
+ * Utiliza el algoritmo BCrypt con un valor aleatorio (salt) integrado para cada hashing.
+ *
+ * @return instancia de PasswordEncoder configurada con BCrypt
+ */
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
