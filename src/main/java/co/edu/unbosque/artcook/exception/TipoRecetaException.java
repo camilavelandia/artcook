b512eacc564c2cc65a8e657003c0ec017aeed903
juanfe11 <64513341+juanfe11@users.noreply.github.com>
2026-05-14 11:45:54 -0500

    Update TipoRecetaException.java
    version unificada cami-juan

--- src/main/java/co/edu/unbosque/artcook/exception/TipoRecetaException.java
+++ src/main/java/co/edu/unbosque/artcook/exception/TipoRecetaException.java
@@ -1,14 +1,16 @@
 package co.edu.unbosque.artcook.exception;
 
+/**
+ * Excepción cuando el tipo de receta es inválido o nulo.
+ */
 public class TipoRecetaException extends Exception {
-	
-	private static final long serialVersionUID = 1L;
-	
-	public TipoRecetaException() {
-		super("El tipo de receta es inválido o nulo");
-	}
-	
-	public TipoRecetaException(String mensaje) {
-		super(mensaje);
-	}
+    private static final long serialVersionUID = 1L;
+
+    public TipoRecetaException() {
+        super("El tipo de receta debe ser COCINA o MANUALIDAD.");
+    }
+
+    public TipoRecetaException(String mensaje) {
+        super(mensaje);
+    }
 }
