4242b7ea37618863eb978b76626bd28e86be0253
juanfe11 <64513341+juanfe11@users.noreply.github.com>
2026-05-14 11:32:42 -0500

    Update ContrasenaException.java
    versión unificada cami-juan

--- src/main/java/co/edu/unbosque/artcook/exception/ContrasenaException.java
+++ src/main/java/co/edu/unbosque/artcook/exception/ContrasenaException.java
@@ -1,16 +1,16 @@
-// clase modificada camila-juan
-
 package co.edu.unbosque.artcook.exception;
 
+/**
+ * Excepción cuando la contraseña no cumple los requisitos mínimos.
+ */
 public class ContrasenaException extends Exception {
-	
-	private static final long serialVersionUID = 1L;
-	
-	public ContrasenaException() {
-		super("La contraseña no cumple con los requisitos");
-	}
-	
-	public ContrasenaException(String mensaje) {
-		super(mensaje);
-	}
+    private static final long serialVersionUID = 1L;
+
+    public ContrasenaException() {
+        super("La contrasena debe tener minimo 8 caracteres, al menos un numero y un caracter especial.");
+    }
+
+    public ContrasenaException(String mensaje) {
+        super(mensaje);
+    }
 }
