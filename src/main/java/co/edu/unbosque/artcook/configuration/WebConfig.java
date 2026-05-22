8a32897d2d430cdfb0e0b36f3c0027dfd636390f
Deiby775 <djforero@unbosque.edu.co>
2026-05-22 01:17:59 -0500

    Create Class for videos and update IA controller and service
    se crearon clases para la creacion de videos, se modificaron clases para la integracios y se cambiaron propiedades

---
+++ src/main/java/co/edu/unbosque/artcook/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package co.edu.unbosque.artcook.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Value("${app.videos.path}")
+	private String videosPath;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/videos/**")
+				.addResourceLocations("file:" + Paths.get(videosPath).toAbsolutePath() + "/");
+	}
+}
